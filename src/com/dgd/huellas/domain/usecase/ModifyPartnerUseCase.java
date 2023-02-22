f3a5df2ab5cdc442ba1c0a1b67514dd847fc588e
diegogd04 <diego2612004@gmail.com>
2023-02-23 00:27:52 +0100

    Crear usecase ModifyPartner

---
+++ src/com/dgd/huellas/domain/usecase/ModifyPartnerUseCase.java
@@ -0,0 +1,12 @@
+package com.dgd.huellas.domain.usecase;
+
+import com.dgd.huellas.data.local.PartnerFileLocalDataSource;
+import com.dgd.huellas.domain.models.Partner;
+
+public class ModifyPartnerUseCase {
+    private PartnerFileLocalDataSource dataSource = PartnerFileLocalDataSource.getInstance();
+
+    public void execute(Partner partner){
+        dataSource.save(partner);
+    }
+}

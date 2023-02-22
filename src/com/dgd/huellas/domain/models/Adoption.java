f179947728b196b2259d36b9052c06b8339899ac
diegogd04 <diego2612004@gmail.com>
2023-02-22 01:34:00 +0100

    Crear clase Adoption

---
+++ src/com/dgd/huellas/domain/models/Adoption.java
@@ -0,0 +1,22 @@
+package com.dgd.huellas.domain.models;
+
+public class Adoption {
+    private Integer id;
+    private String adoptionDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAdoptionDate() {
+        return adoptionDate;
+    }
+
+    public void setAdoptionDate(String adoptionDate) {
+        this.adoptionDate = adoptionDate;
+    }
+}

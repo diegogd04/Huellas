da44589ac3c4b52bc6afb1fffe00c4b2cbe4981b
diegogd04 <diego2612004@gmail.com>
2023-02-22 01:31:30 +0100

    Crear clase Socio

---
+++ src/com/dgd/huellas/domain/models/Socio.java
@@ -0,0 +1,49 @@
+package com.dgd.huellas.domain.models;
+
+public class Socio {
+    private Integer id;
+    private String name;
+    private String surnames;
+    private String dni;
+    private String bankAccount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurnames() {
+        return surnames;
+    }
+
+    public void setSurnames(String surnames) {
+        this.surnames = surnames;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getBankAccount() {
+        return bankAccount;
+    }
+
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+}

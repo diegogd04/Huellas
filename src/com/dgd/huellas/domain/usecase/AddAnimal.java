241fe01df964067dece570d57979d1c1c6a2e29d
diegogd04 <diego2612004@gmail.com>
2023-02-22 02:09:09 +0100

    Crear usecase AddAnimal

---
+++ src/com/dgd/huellas/domain/usecase/AddAnimal.java
@@ -0,0 +1,12 @@
+package com.dgd.huellas.domain.usecase;
+
+import com.dgd.huellas.data.local.AnimalFileLocalDataSource;
+import com.dgd.huellas.domain.models.Animal;
+
+public class AddAnimal {
+    private AnimalFileLocalDataSource dataSource = AnimalFileLocalDataSource.getInstance();
+
+    public void execute(Animal animal) {
+        dataSource.save(animal);
+    }
+}
